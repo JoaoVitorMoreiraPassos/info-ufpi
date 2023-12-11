e4446d745bcdf0dc8f711ec5fb6d05af53f37aa2
dayangomes <dayanramos5537@gmail.com>
2023-12-11 14:53:26 -0300

    feat: add campo favorito no model post

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     categoria_post = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, blank=True, null=True, verbose_name='Categoria')
     imagem_post = models.ImageField(upload_to='post_img/%Y/%m/%d', blank=True, null=True, verbose_name='Imagem')
     publicado_post = models.BooleanField(default=False, verbose_name='Publicado')
+    favorito_post = models.ManyToManyField(User, related_name='favorito', verbose_name='Favorito', blank=True)
 
     def __str__(self):
         return self.titulo_post
