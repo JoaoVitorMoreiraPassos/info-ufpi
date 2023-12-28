43e68887d68f7da896af915a72a90379ed7a6758
JoaoVitorMoreiraPassos <moreirapassosj@gmail.com>
2023-12-27 21:29:53 -0300

    feat: Criação da página de noticia.

--- frontend/pages/noticias/[...slug].tsx
+++ frontend/pages/noticias/[...slug].tsx
@@ -1,38 +1,40 @@
+import React from 'react';
+import SideBar from '@/app/components/SideBar';
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
+import NoticeContent from '@/app/components/MainContent/NoticeContent';
 import { useRouter } from 'next/router';
-import { metadata } from '@/app/layout';
+import '@/app/globals.css';
+import { Inria_Serif } from 'next/font/google';
 
-interface Noticia {
-    titulo: string;
-    descricao: string;
-    imagem: string;
-    data: string;
-    autor: string;
-    categoria: string;
-}
+const inria_serif = Inria_Serif({ subsets: ['latin'], weight: ["300"] })
 
-const NoticiasPage = () => {
+export default function Notice() {
+
     const router = useRouter();
     const { slug } = router.query;
 
-    const noticia = async () => {
-        const response = await fetch('http://localhost:3333/noticias/' + slug)
-        const data: Noticia = await response.json()
-        return data
-    }
+    React.useEffect(() => {
+        // Modifique o metadata após a renderização inicial
+        document.title = slug as string;
+    });
 
 
     return (
-        < div >
-            <h1>Notícias</h1>
-            {
-                slug && (
-                    <p>
-                        Slug: {Array.isArray(slug) ? slug.join('/') : slug}
-                    </p>
-                )
-            }
-        </div >
+        <div data-theme='light' className={inria_serif.className}>
+            <div className="flex min-h-screen flex-row justify-start">
+                <SideBar />
+                <div className='mainContainer border-l'>
+                    <Header page_index={0} />
+                    <main>
+                        <NoticeContent slug={slug as string} />
+                    </main>
+                </div>
+            </div >
+            <footer>
+                <Footer />
+            </footer>
+        </div>
     );
 };
 
-export default NoticiasPage;
