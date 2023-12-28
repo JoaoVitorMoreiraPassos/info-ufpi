63eaf0da4a4f5223196fa4c84f87fbe29fefb6b0
JoaoVitorMoreiraPassos <moreirapassosj@gmail.com>
2023-12-27 21:32:22 -0300

    refactor: refatorando layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,16 @@
 
-import type { Metadata } from 'next'
-import './globals.css'
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import Footer from './components/Footer'
-import { Inria_Serif, Inter } from 'next/font/google';
 import { config } from "@fortawesome/fontawesome-svg-core";
-import SideBar from './components/SideBar'
-import Header from './components/Header'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { Inter } from 'next/font/google';
+import './globals.css'
+import type { Metadata } from 'next'
 // Tell Font Awesome to skip adding the CSS automatically 
 // since it's already imported above
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] })
 
-export let metadata: Metadata = {
+export const metadata: Metadata = {
   title: '',
   description: '',
 }
