95d26a51b6f2302465d6c9bf6a5d248109b52d39
JoaoVitorMoreiraPassos <moreirapassosj@gmail.com>
2023-12-27 21:27:28 -0300

    feat: Criação da página de contatos

---
+++ frontend/app/components/MainContent/ContactContent/index.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React from 'react';
+import './style.css'
+
+const ContactContent = () => {
+    const [email, setEmail] = React.useState('');
+    const [message, setMessage] = React.useState('');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(email, message);
+    }
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    }
+    const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.target.value);
+    }
+
+    return (
+        <div className='w-full h-4/5 flex flex-col justify-center items-center'>
+            <h1 className='text-4xl  md:w-2/3 lg:max-w-2xl max-md:text-2xl text-center'>
+                Procurando ajuda? Entre em contato!
+            </h1>
+            <form onSubmit={handleSubmit} className=' md:w-2/3 flex flex-col gap-3 lg:max-w-2xl max-md:text-2xl text-center max-sm:w-full max-sm:p-2'>
+                <input type="email" className='w-full' placeholder='E-mail *' onChange={handleEmailChange} />
+                <textarea className='w-full resize-none h-80' placeholder='Mensagem *' onChange={handleMessageChange} />
+                <div className='flex justify-end'>
+                    <button
+                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 w-48 rounded-2xl'
+                        type='submit'>Enviar</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default ContactContent
