4cf0775a7da18c6e976c7853051a33a83e3327be
JoaoVitorMoreiraPassos <moreirapassosj@gmail.com>
2024-01-07 15:52:24 -0300

    feat: integrando o formulário de login com a api

--- frontend/app/components/LoginForm/index.tsx
+++ frontend/app/components/LoginForm/index.tsx
@@ -8,7 +8,7 @@ const LoginForm = () => {
     const [usernameError, setUsernameError] = useState('');
     const [passwordError, setPasswordError] = useState('');
 
-    const handleSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         // Limpar mensagens de erro
@@ -22,27 +22,61 @@ const LoginForm = () => {
             return;
         }
 
-        if (!username.includes('@') || !username.includes('.')) {
-            document.getElementById('username')?.focus();
-            setUsernameError('Por favor, insira um e-mail ou telefone válido.');
-            return;
-        }
-
         if (!password) {
             document.getElementById('passwordInput')?.focus();
             setPasswordError('Por favor, insira sua senha.');
             return;
         }
+
+        let data = {
+            username,
+            password
+        }
+
+        let url = 'http://localhost:8000/login/'
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+
+            },
+            mode: 'cors'
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                if (data.status === 'success') {
+                    console.log("deu certo")
+                    let token = data.token
+                    let user = data.user
+                    localStorage.setItem('token', token)
+                    localStorage.setItem('user', JSON.stringify(user))
+                    window.location.href = '/'
+                }
+                else if (data.status === 'error') {
+                    setUsernameError(data.message)
+                }
+                else if (data.detail === "Não encontrado.") {
+                    setUsernameError('Usuário não encontrado.')
+                }
+            })
+
+            .catch((error) => {
+                console.log(error)
+            });
+
     }
 
     return (
-        <form className='flex flex-col p-6 gap-6 w-1/1 items-center'>
+        <form className='flex flex-col p-6 gap-6 w-1/1 items-center' onSubmit={handleSubmit}>
             <input
                 className={`rounded-lg ${usernameError ? 'border-red-500' : ''}`}
-                type="email"
+                type="text"
                 name="username"
                 id="username"
-                placeholder="E-mail ou telefone"
+                placeholder="Usuário"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
             />
@@ -58,7 +92,6 @@ const LoginForm = () => {
 
             <button
                 type="submit"
-                onClick={handleSubmit}
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-40"
             >
                 Entrar
