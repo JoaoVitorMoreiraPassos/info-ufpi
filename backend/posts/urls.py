d35df902bf27f3c43fdf94afe9fbc6589f6e1cff
dayangomes <dayanramos5537@ufpi.edu.br>
2024-01-18 17:34:29 -0300

    feat: Configurando rotas de favoritos

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -5,6 +5,10 @@ from .views import (
     PostAPIView,
     ComentariosAPIView,
     ComentarioAPIView,
+    AddFavoritoAPIView,
+    DeleteFavoritoAPIView,
+    FavoritosAPIView,
+
 )
 
 urlpatterns = [
@@ -15,6 +19,10 @@ urlpatterns = [
     path("posts/<int:post_pk>/comentarios/<int:pk>/",
          ComentarioAPIView.as_view(), name="comentario"),
 
+    path("favoritos/<int:post_id>/", AddFavoritoAPIView.as_view(), name="favorito"),
+    path("favoritos/delete/<int:post_id>/",
+         DeleteFavoritoAPIView.as_view(), name="delete_favorito"),
+    path("favoritos/", FavoritosAPIView.as_view(), name="favoritos"),
 
     # Inutil por enquanto
     # path("comentarios/", ComentariosAPIView.as_view(), name="comentarios"),
