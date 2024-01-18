41299b37c848dc0a65423dfc59baff25bbf6eb76
dayangomes <dayanramos5537@ufpi.edu.br>
2024-01-18 17:35:45 -0300

    fix: COlocando o campo email como campo obrigatorio no cadstro de usuários

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -5,5 +5,7 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name',
+    list_display = ('id', 'username', 'first_name', 'last_name',
                     'email', 'is_staff', 'foto_perfil', 'post_permissoes')
+    list_display_links = ('id', 'username', 'first_name', 'last_name',
+                          'email', 'foto_perfil')
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class User(AbstractUser):
     # ... outros campos ...
+    email = models.EmailField('Email Address', unique=True, blank=False, null=False)
     foto_perfil = models.ImageField(upload_to='fotos_perfil/', null=True, blank=True)
     post_permissoes = models.BooleanField(default=False, verbose_name='Permissao de Postagem')
 
