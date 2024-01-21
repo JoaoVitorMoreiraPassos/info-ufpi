de6d42ded05e0813ea9168aca7bbc51fa0e6605f
JoaoVitorMoreiraPassos <moreirapassosj@gmail.com>
2024-01-20 22:11:10 -0300

    feat: Integrando a api para realizar a ações do restaurante universitário

--- frontend/app/components/RecipeDelete/index.tsx
+++ frontend/app/components/RecipeDelete/index.tsx
@@ -2,22 +2,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
-
+import api from '@/app/api/ru'
 interface Item {
     id: number,
-    name: string,
-    type: string
+    nome_refeicao: string,
+    tipo_refeicao: string
 }
 
 const RecipeDelete: React.FC<{ item: Item }> = ({ item }) => {
 
-    const deleteItem = () => {
-        document.querySelector(`#${item.type + item.id}`)?.remove()
+    const deleteItem = async () => {
+
+        const response = await api.deleteAlimento(item.id);
+
+        const node = document.querySelector(`#${item.tipo_refeicao}${item.id}`)
+        if (node) {
+            if (node.parentNode) {
+                node.parentNode.removeChild(node);
+            }
+        }
     }
 
     return (
-        <div key={item.id} id={item.type + item.id} className='flex flex-row justify-between items-center w-80  border-b-2 border-solid p-1' >
-            <p>{item.name}</p>
+        <div key={item.id} id={item.tipo_refeicao + item.id} className='flex flex-row justify-between items-center w-80  border-b-2 border-solid p-1' >
+            <p>{item.nome_refeicao}</p>
             <button className=' text-red-500 rounded-xl p-2 text-xl flex items-center justify-center' onClick={
                 () => {
                     deleteItem()
@@ -28,3 +36,4 @@ const RecipeDelete: React.FC<{ item: Item }> = ({ item }) => {
 }
 
 export default RecipeDelete
+
