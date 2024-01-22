5473eefd82bccf2b01e34e6cb4a0c6ab235ebfbf
JoaoVitorMoreiraPassos <moreirapassosj@gmail.com>
2024-01-22 04:08:47 -0300

    feat: Adicionando rotas para pesquisa de posts

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -8,17 +8,18 @@ from .views import (
     AddFavoritoAPIView,
     DeleteFavoritoAPIView,
     FavoritosAPIView,
-
+     SearchAPIView,
+     SearchPostByAutorAPIView
 )
 
 urlpatterns = [
     path("posts/", PostsAPIView.as_view(), name="posts"),
     path("posts/<int:pk>/", PostAPIView.as_view(), name="post"),
+    path("posts/search/<str:search>/", SearchPostByAutorAPIView.as_view(), name="search"),
     path("posts/<int:post_pk>/comentarios/",
          ComentariosAPIView.as_view(), name="comentarios"),
     path("posts/<int:post_pk>/comentarios/<int:pk>/",
          ComentarioAPIView.as_view(), name="comentario"),
-
     path("favoritos/<int:post_id>/", AddFavoritoAPIView.as_view(), name="favorito"),
     path("favoritos/delete/<int:post_id>/",
          DeleteFavoritoAPIView.as_view(), name="delete_favorito"),
