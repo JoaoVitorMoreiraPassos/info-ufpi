3298dde8dbff06a9e1ad294be6163c0b63a126a2
João Vitor <49813644+JoaoVitorMoreiraPassos@users.noreply.github.com>
2024-01-22 04:22:41 -0300

    Update settings.py

--- backend/info_ufpi/settings.py
+++ backend/info_ufpi/settings.py
@@ -202,6 +202,6 @@ DOMAIN = 'localhost:3000'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'joseegidio11@gmail.com'
-EMAIL_HOST_PASSWORD = 'ewhq zykm sxnq jgdr'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
